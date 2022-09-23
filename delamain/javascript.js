56223f3d6874a8d2374dacc52dbbdc5736eb43aa
rjblack0 <rjblack0@live.com>
2022-09-22 23:02:03 -0500

    Added practice websites htmlcss plus javascript practice

---
+++ delamain/javascript.js
@@ -0,0 +1,12 @@
+
+let user = "John",
+    age = 25;
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+
+alert( `Hello, ${user}!` );
